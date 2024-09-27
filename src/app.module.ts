0152552ec684f0305c5c4b30b69e74729b25f3a4
Simeon Kostadinov <simeon.kostadinov0141@gmail.com>
2024-09-27 23:39:34 +0200

    added mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import * as process from 'node:process';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(
+      `mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
+    ),
+  ],
   controllers: [],
   providers: [],
 })
