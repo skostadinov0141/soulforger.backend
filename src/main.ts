8206d9a77c9bdea8ae0288d97381123e01617b58
Simeon Kostadinov <simeon.kostadinov0141@gmail.com>
2024-09-27 23:23:31 +0200

    added deployment added dev environment added swagger removed unneeded files

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Configure Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Soulforger API')
+    .setDescription('The Soulforger API description')
+    .setVersion('0.0.1')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
