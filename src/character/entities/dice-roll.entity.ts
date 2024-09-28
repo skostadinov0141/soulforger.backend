8a12612a08a6d864cb708ee7d505cac84489f5ea
Simeon Kostadinov <simeon.kostadinov0141@gmail.com>
2024-09-29 00:07:04 +0200

    added dice-roll.entity.ts

---
+++ src/character/entities/dice-roll.entity.ts
@@ -0,0 +1,36 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { ApiProperty } from '@nestjs/swagger';
+import { Rulebook } from '../../rulebook/entities/rulebook.entity';
+import mongoose from 'mongoose';
+
+@Schema()
+export class DiceRoll {
+  @ApiProperty()
+  _id: string;
+
+  @ApiProperty()
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: Rulebook.name })
+  rulebook: Rulebook;
+
+  @ApiProperty()
+  @Prop()
+  name: string;
+
+  @ApiProperty()
+  @Prop()
+  diceSides: number;
+
+  @ApiProperty()
+  @Prop()
+  diceCount: number;
+
+  @ApiProperty()
+  @Prop()
+  modifier: number;
+
+  @ApiProperty()
+  @Prop()
+  result: number;
+}
+
+export const DiceRollEntity = SchemaFactory.createForClass(DiceRoll);
