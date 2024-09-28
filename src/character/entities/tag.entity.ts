94baf197e9eefac7d8d914cd0e09653726dd3461
Simeon Kostadinov <simeon.kostadinov0141@gmail.com>
2024-09-29 01:05:48 +0200

    added tag.entity.ts

---
+++ src/character/entities/tag.entity.ts
@@ -0,0 +1,24 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { ApiProperty } from '@nestjs/swagger';
+import { Rulebook } from '../../rulebook/entities/rulebook.entity';
+import mongoose from 'mongoose';
+
+@Schema()
+export class Tag {
+  @ApiProperty()
+  _id: string;
+
+  @ApiProperty()
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: Rulebook.name })
+  rulebook: Rulebook;
+
+  @ApiProperty()
+  @Prop()
+  name: string;
+
+  @ApiProperty()
+  @Prop()
+  description: string;
+}
+
+export const TagSchema = SchemaFactory.createForClass(Tag);
