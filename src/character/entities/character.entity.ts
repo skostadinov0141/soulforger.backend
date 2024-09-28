6dfd4488fa99bc136fed271c157758b9413daffb
Simeon Kostadinov <simeon.kostadinov0141@gmail.com>
2024-09-29 01:39:27 +0200

    added cascade style delete in character.entity.ts

--- src/character/entities/character.entity.ts
+++ src/character/entities/character.entity.ts
@@ -2,8 +2,8 @@ import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { ApiProperty } from '@nestjs/swagger';
 import { Rulebook } from '../../rulebook/entities/rulebook.entity';
 import mongoose from 'mongoose';
-import { Attribute } from './attribute.entity';
-import { Ability } from './ability.entity';
+import { Attribute, AttributeSchema } from './attribute.entity';
+import { Ability, AbilitySchema } from './ability.entity';
 
 @Schema()
 export class Character {
@@ -29,4 +29,14 @@ export class Character {
   abilities: Ability[];
 }
 
-export const CharacterSchema = SchemaFactory.createForClass(Character);
+const CharacterSchema = SchemaFactory.createForClass(Character).pre(
+  'deleteOne',
+  { document: true },
+  function (next) {
+    AttributeSchema.remove({ _id: { $in: this.attributes } });
+    AbilitySchema.remove({ _id: { $in: this.abilities } });
+    next();
+  },
+);
+
+export { CharacterSchema };
