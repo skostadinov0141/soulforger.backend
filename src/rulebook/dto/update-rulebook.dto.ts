cdd27fc7cd41857793de3b282074b271e4b7307e
Simeon Kostadinov <simeon.kostadinov0141@gmail.com>
2024-09-28 23:37:49 +0200

    added validation finished create and update rulebook dto

--- src/rulebook/dto/update-rulebook.dto.ts
+++ src/rulebook/dto/update-rulebook.dto.ts
@@ -1,4 +1,20 @@
-import { PartialType } from '@nestjs/swagger';
+import { ApiProperty, PartialType } from '@nestjs/swagger';
 import { CreateRulebookDto } from './create-rulebook.dto';
+import { IsString, Length } from 'class-validator';
 
-export class UpdateRulebookDto extends PartialType(CreateRulebookDto) {}
+export class UpdateRulebookDto extends PartialType(CreateRulebookDto) {
+  @ApiProperty()
+  @IsString()
+  @Length(2, 64)
+  owner: string;
+
+  @ApiProperty()
+  @IsString()
+  @Length(2, 128)
+  name: string;
+
+  @ApiProperty()
+  @IsString()
+  @Length(2, 2048)
+  description: string;
+}
