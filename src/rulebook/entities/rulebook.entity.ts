876b5b607c30a3d8eb627390faa41e77dc52da7e
Simeon Kostadinov <simeon.kostadinov0141@gmail.com>
2024-09-29 00:01:27 +0200

    finished all controller endpoints in rulebook.controller.ts

--- src/rulebook/entities/rulebook.entity.ts
+++ src/rulebook/entities/rulebook.entity.ts
@@ -1,15 +1,20 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { ApiProperty } from '@nestjs/swagger';
 
 @Schema()
 export class Rulebook {
+  @ApiProperty()
   _id: string;
 
+  @ApiProperty()
   @Prop()
   owner: string;
 
+  @ApiProperty()
   @Prop()
   name: string;
 
+  @ApiProperty()
   @Prop()
   description: string;
 }
--- src/rulebook/rulebook.controller.ts
+++ src/rulebook/rulebook.controller.ts
@@ -10,27 +10,50 @@ import {
 import { RulebookService } from './rulebook.service';
 import { CreateRulebookDto } from './dto/create-rulebook.dto';
 import { UpdateRulebookDto } from './dto/update-rulebook.dto';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
+import { Rulebook } from './entities/rulebook.entity';
 
+@ApiTags('rulebook')
 @Controller('rulebook')
 export class RulebookController {
   constructor(private readonly rulebookService: RulebookService) {}
 
   @Post()
+  @ApiResponse({
+    status: 201,
+    description: 'The record was successfully created.',
+    type: Rulebook,
+  })
   create(@Body() createRulebookDto: CreateRulebookDto) {
     return this.rulebookService.create(createRulebookDto);
   }
 
   @Get()
+  @ApiResponse({
+    status: 200,
+    description: 'The records were successfully retrieved.',
+    type: [Rulebook],
+  })
   findAll() {
     return this.rulebookService.findAll();
   }
 
   @Get(':id')
+  @ApiResponse({
+    status: 200,
+    description: 'The record was successfully retrieved.',
+    type: Rulebook,
+  })
   findOne(@Param('id') id: string) {
     return this.rulebookService.findOne(+id);
   }
 
   @Patch(':id')
+  @ApiResponse({
+    status: 200,
+    description: 'The record was successfully updated.',
+    type: Rulebook,
+  })
   update(
     @Param('id') id: string,
     @Body() updateRulebookDto: UpdateRulebookDto,
@@ -39,6 +62,11 @@ export class RulebookController {
   }
 
   @Delete(':id')
+  @ApiResponse({
+    status: 200,
+    description: 'The record was successfully deleted.',
+    type: Rulebook,
+  })
   remove(@Param('id') id: string) {
     return this.rulebookService.remove(+id);
   }
