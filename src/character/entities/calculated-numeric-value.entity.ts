a4e548e2d0fd2602b09412b44a9984359616d1e0
Simeon Kostadinov <simeon.kostadinov0141@gmail.com>
2024-09-29 00:23:44 +0200

    added calculated-numeric-value.entity.ts

---
+++ src/character/entities/calculated-numeric-value.entity.ts
@@ -0,0 +1,46 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { ApiProperty } from '@nestjs/swagger';
+import { Rulebook } from '../../rulebook/entities/rulebook.entity';
+import mongoose from 'mongoose';
+import { CharacterFieldPath } from './character-field-path.entity';
+import { DiceRoll } from './dice-roll.entity';
+
+@Schema()
+export class CalculatedNumericValue {
+  @ApiProperty()
+  _id: string;
+
+  @ApiProperty()
+  @Prop()
+  type: 'CalculatedNumericValue';
+
+  @ApiProperty()
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: Rulebook.name })
+  rulebook: Rulebook;
+
+  @ApiProperty()
+  @Prop({
+    type: [
+      { type: mongoose.Schema.Types.ObjectId, ref: CharacterFieldPath.name },
+    ],
+  })
+  variables: CharacterFieldPath[];
+
+  @ApiProperty()
+  @Prop({
+    type: [{ type: mongoose.Schema.Types.ObjectId, ref: DiceRoll.name }],
+  })
+  diceRolls: DiceRoll[];
+
+  @ApiProperty()
+  @Prop()
+  formula: string;
+
+  @ApiProperty()
+  @Prop()
+  value: number;
+}
+
+export const CalculatedNumericValueEntity = SchemaFactory.createForClass(
+  CalculatedNumericValue,
+);
