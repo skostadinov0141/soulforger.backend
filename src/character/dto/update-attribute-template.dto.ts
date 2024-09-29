d5a1634e7fcacd98bb86fad4f26b2a8e1f5f680f
Simeon Kostadinov <simeon.kostadinov0141@gmail.com>
2024-09-29 15:23:47 +0200

    added update-attribute-template.dto.ts

---
+++ src/character/dto/update-attribute-template.dto.ts
@@ -0,0 +1,124 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEnum, IsNumber, IsString } from 'class-validator';
+
+export class UpdateAttributeTemplateDto {
+  @ApiProperty()
+  @IsString()
+  _id: string;
+
+  @ApiProperty()
+  @IsString()
+  rulebook: string;
+
+  @ApiProperty()
+  @IsString()
+  name: string;
+
+  @ApiProperty()
+  @IsString()
+  description: string;
+
+  @ApiProperty()
+  @IsString()
+  @IsEnum(['FixedNumericValue', 'TextValue', 'CalculatedNumericValue'])
+  attributeType: string;
+
+  @ApiProperty()
+  @IsString()
+  attributeValue:
+    | UpdateTextValueTemplateDto
+    | UpdateFixedNumericValueTemplateDto
+    | UpdateCalculatedNumericValueTemplateDto;
+}
+
+export class UpdateTextValueTemplateDto {
+  @ApiProperty()
+  @IsString()
+  _id: string;
+
+  @ApiProperty()
+  @IsString()
+  rulebook: string;
+
+  @ApiProperty()
+  @IsString()
+  value: string;
+
+  @ApiProperty()
+  @IsString()
+  options: string[];
+}
+
+export class UpdateFixedNumericValueTemplateDto {
+  @ApiProperty()
+  @IsString()
+  _id: string;
+
+  @ApiProperty()
+  @IsString()
+  rulebook: string;
+
+  @ApiProperty()
+  @IsString()
+  value: string;
+}
+
+export class UpdateCalculatedNumericValueTemplateDto {
+  @ApiProperty()
+  @IsString()
+  _id: string;
+
+  @ApiProperty()
+  @IsString()
+  rulebook: string;
+
+  @ApiProperty()
+  @IsString()
+  formula: string;
+
+  @ApiProperty()
+  variables: UpdateCharacterFieldPathTemplateDto[];
+
+  @ApiProperty()
+  diceRolls: UpdateDiceRollTemplateDto[];
+}
+
+export class UpdateCharacterFieldPathTemplateDto {
+  @ApiProperty()
+  @IsString()
+  _id: string;
+
+  @ApiProperty()
+  @IsString()
+  rulebook: string;
+
+  @ApiProperty()
+  @IsString()
+  name: string;
+
+  @ApiProperty()
+  @IsString()
+  path: string;
+}
+
+export class UpdateDiceRollTemplateDto {
+  @ApiProperty()
+  @IsString()
+  _id: string;
+
+  @ApiProperty()
+  @IsString()
+  rulebook: string;
+
+  @ApiProperty()
+  @IsString()
+  name: string;
+
+  @ApiProperty()
+  @IsNumber()
+  diceSides: number;
+
+  @ApiProperty()
+  @IsNumber()
+  diceAmount: string;
+}
