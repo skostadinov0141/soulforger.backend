4d3f42301d509ce9ede93690ede4be0871bc1053
Simeon Kostadinov <simeon.kostadinov0141@gmail.com>
2024-10-06 02:54:37 +0200

    added mongoose module to group.module.ts

--- src/group/group.module.ts
+++ src/group/group.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { GroupService } from './group.service';
 import { GroupController } from './group.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Group, GroupSchema } from './entities/group.entity';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Group.name, schema: GroupSchema }]),
+  ],
   controllers: [GroupController],
   providers: [GroupService],
 })
