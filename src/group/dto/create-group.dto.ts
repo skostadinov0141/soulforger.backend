48b9ba18e230b3855024f05fcc9cc2d79a332619
Simeon Kostadinov <simeon.kostadinov0141@gmail.com>
2024-10-06 02:59:27 +0200

    added create-group.dto.ts

--- src/group/dto/create-group.dto.ts
+++ src/group/dto/create-group.dto.ts
@@ -1,1 +1,21 @@
-export class CreateGroupDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEnum, IsString } from 'class-validator';
+
+export class CreateGroupDto {
+  @ApiProperty()
+  @IsString()
+  rulebook: string;
+
+  @ApiProperty()
+  @IsString()
+  name: string;
+
+  @ApiProperty()
+  @IsString()
+  description: string;
+
+  @ApiProperty()
+  @IsString()
+  @IsEnum(['attribute', 'ability'])
+  for: string;
+}
