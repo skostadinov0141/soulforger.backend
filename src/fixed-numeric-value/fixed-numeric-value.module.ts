b5ef31e4a956f5b453b0981cfd797f9330c74370
Simeon Kostadinov <simeon.kostadinov0141@gmail.com>
2024-10-07 14:03:28 +0200

    added needed import/exports to fixed-numeric-value.module.ts

--- src/fixed-numeric-value/fixed-numeric-value.module.ts
+++ src/fixed-numeric-value/fixed-numeric-value.module.ts
@@ -1,7 +1,20 @@
 import { Module } from '@nestjs/common';
 import { FixedNumericValueService } from './fixed-numeric-value.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import {
+  FixedNumericValue,
+  FixedNumericValueSchema,
+} from './entities/fixed-numeric-value.entity';
+import { RulebookModule } from '../rulebook/rulebook.module';
 
 @Module({
-  providers: [FixedNumericValueService]
+  providers: [FixedNumericValueService],
+  imports: [
+    MongooseModule.forFeature([
+      { name: FixedNumericValue.name, schema: FixedNumericValueSchema },
+    ]),
+    RulebookModule,
+  ],
+  exports: [FixedNumericValueService, MongooseModule],
 })
 export class FixedNumericValueModule {}
