e8a8d51f687cbef39fc3cc40aad08cd703471aa2
Simeon Kostadinov <simeon.kostadinov0141@gmail.com>
2024-10-07 14:05:34 +0200

    added needed injections to fixed-numeric-value.service.ts

--- src/fixed-numeric-value/fixed-numeric-value.service.ts
+++ src/fixed-numeric-value/fixed-numeric-value.service.ts
@@ -1,4 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { FixedNumericValue } from './entities/fixed-numeric-value.entity';
+import { Model } from 'mongoose';
+import { RulebookService } from '../rulebook/rulebook.service';
+import { I18nService } from 'nestjs-i18n';
 
 @Injectable()
-export class FixedNumericValueService {}
+export class FixedNumericValueService {
+  constructor(
+    @InjectModel(FixedNumericValue.name)
+    private readonly fixedNumericValueModel: Model<FixedNumericValue>,
+    @Inject() private readonly rulebookService: RulebookService,
+    private readonly i18n: I18nService,
+  ) {}
+}
