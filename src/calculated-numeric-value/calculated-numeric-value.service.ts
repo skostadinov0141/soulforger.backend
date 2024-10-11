3a4630edbade01e4fd2db2110ebb41cfb62d4d66
Simeon Kostadinov <simeon.kostadinov0141@gmail.com>
2024-10-11 16:05:18 +0200

    injected needed models and services into calculated-numeric-value.service.ts added translate function to calculated-numeric-value.service.ts

--- src/calculated-numeric-value/calculated-numeric-value.service.ts
+++ src/calculated-numeric-value/calculated-numeric-value.service.ts
@@ -1,4 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { CalculatedNumericValue } from './entities/calculated-numeric-value.entity';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { DiceRollService } from '../dice-roll/dice-roll.service';
+import { RulebookService } from '../rulebook/rulebook.service';
+import { CharacterFieldPathService } from '../character-field-path/character-field-path.service';
+import { I18nContext, I18nService } from 'nestjs-i18n';
 
 @Injectable()
-export class CalculatedNumericValueService {}
+export class CalculatedNumericValueService {
+  constructor(
+    @InjectModel(CalculatedNumericValue.name)
+    private readonly calculatedNumericValueModel: Model<CalculatedNumericValue>,
+    @Inject() private readonly rulebookService: RulebookService,
+    @Inject()
+    private readonly characterFieldPathService: CharacterFieldPathService,
+    @Inject() private readonly diceRollService: DiceRollService,
+    @Inject() private readonly i18n: I18nService,
+  ) {}
+
+  private translate(path: string) {
+    return this.i18n.t(path, { lang: I18nContext.current().lang });
+  }
+}
