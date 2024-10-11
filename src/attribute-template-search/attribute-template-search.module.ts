2992cee908531b771c66bcee8e37e65a68eafe90
Simeon Kostadinov <simeon.kostadinov0141@gmail.com>
2024-10-11 22:01:04 +0200

    imported and exported needed modules in attribute-template-search.module.ts

--- src/attribute-template-search/attribute-template-search.module.ts
+++ src/attribute-template-search/attribute-template-search.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AttributeTemplateSearchService } from './attribute-template-search.service';
+import { AttributeTemplateModule } from '../attribute-template/attribute-template.module';
+import { RulebookModule } from '../rulebook/rulebook.module';
 
 @Module({
-  providers: [AttributeTemplateSearchService]
+  providers: [AttributeTemplateSearchService],
+  imports: [AttributeTemplateModule, RulebookModule],
+  exports: [AttributeTemplateSearchService],
 })
 export class AttributeTemplateSearchModule {}
