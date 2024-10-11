6b802dce6530fd69b7a1f0c006b007fb275457e2
Simeon Kostadinov <simeon.kostadinov0141@gmail.com>
2024-10-11 16:37:58 +0200

    imported needed services and mongoose modules in calculated-numeric-value.module.ts

--- src/calculated-numeric-value/calculated-numeric-value.module.ts
+++ src/calculated-numeric-value/calculated-numeric-value.module.ts
@@ -1,7 +1,27 @@
 import { Module } from '@nestjs/common';
 import { CalculatedNumericValueService } from './calculated-numeric-value.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import {
+  CalculatedNumericValue,
+  CalculatedNumericValueSchema,
+} from './entities/calculated-numeric-value.entity';
+import { DiceRollModule } from '../dice-roll/dice-roll.module';
+import { RulebookModule } from '../rulebook/rulebook.module';
+import { CharacterFieldPathModule } from '../character-field-path/character-field-path.module';
 
 @Module({
-  providers: [CalculatedNumericValueService]
+  providers: [CalculatedNumericValueService],
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: CalculatedNumericValue.name,
+        schema: CalculatedNumericValueSchema,
+      },
+    ]),
+    RulebookModule,
+    CharacterFieldPathModule,
+    DiceRollModule,
+  ],
+  exports: [CalculatedNumericValueService, MongooseModule],
 })
 export class CalculatedNumericValueModule {}
