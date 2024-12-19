df3d7db9c746e768f9e0a55b5510be240dcfd3a1
Simeon Kostadinov <simeon.kostadinov0141@gmail.com>
2024-12-19 23:27:17 +0100

    Completed rulebook.controller.ts

--- src/rulebook/rulebook.controller.ts
+++ src/rulebook/rulebook.controller.ts
@@ -1,34 +1,80 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Query,
+} from '@nestjs/common';
 import { RulebookService } from './rulebook.service';
 import { CreateRulebookDto } from './dto/create-rulebook.dto';
 import { UpdateRulebookDto } from './dto/update-rulebook.dto';
+import {
+  ApiBody,
+  ApiOkResponse,
+  ApiParam,
+  ApiQuery,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+import { Rulebook } from './entities/rulebook.entity';
 
 @Controller('rulebook')
+@ApiTags('rulebook')
 export class RulebookController {
   constructor(private readonly rulebookService: RulebookService) {}
 
   @Post()
+  @ApiOkResponse({ type: Rulebook, description: 'Created rulebook' })
+  @ApiBody({ type: CreateRulebookDto })
   create(@Body() createRulebookDto: CreateRulebookDto) {
     return this.rulebookService.create(createRulebookDto);
   }
 
   @Get()
-  findAll() {
-    return this.rulebookService.findAll();
+  @ApiQuery({ name: 'page', required: false, type: Number })
+  @ApiQuery({ name: 'limit', required: false, type: Number })
+  @ApiOkResponse({
+    description: 'List of rulebooks',
+    type: [Rulebook],
+  })
+  findAll(@Query('page') page: number = 0, @Query('limit') limit: number = 10) {
+    return this.rulebookService.findAll(page, limit);
   }
 
   @Get(':id')
+  @ApiOkResponse({
+    description: 'Rulebook details',
+    type: Rulebook,
+  })
+  @ApiParam({ name: 'id', type: String })
   findOne(@Param('id') id: string) {
-    return this.rulebookService.findOne(+id);
+    return this.rulebookService.findOne(id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateRulebookDto: UpdateRulebookDto) {
-    return this.rulebookService.update(+id, updateRulebookDto);
+  @ApiParam({ name: 'id', type: String })
+  @ApiBody({ type: UpdateRulebookDto })
+  @ApiOkResponse({
+    description: 'Updated rulebook',
+    type: Rulebook,
+  })
+  update(
+    @Param('id') id: string,
+    @Body() updateRulebookDto: UpdateRulebookDto,
+  ) {
+    return this.rulebookService.update(id, updateRulebookDto);
   }
 
   @Delete(':id')
+  @ApiParam({ name: 'id', type: String })
+  @ApiOkResponse({
+    description: 'Deleted rulebook',
+    type: Rulebook,
+  })
   remove(@Param('id') id: string) {
-    return this.rulebookService.remove(+id);
+    return this.rulebookService.remove(id);
   }
 }
