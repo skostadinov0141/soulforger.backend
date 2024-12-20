6491d22963f6b45386fc38d12627a566332d3123
Simeon Kostadinov <simeon.kostadinov0141@gmail.com>
2024-12-21 00:32:26 +0100

    added all crud operations for tag in tag.service.ts

--- src/tag/tag.service.ts
+++ src/tag/tag.service.ts
@@ -1,26 +1,81 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Inject, Injectable } from '@nestjs/common';
 import { CreateTagDto } from './dto/create-tag.dto';
 import { UpdateTagDto } from './dto/update-tag.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Tag } from './entities/tag.entity';
+import { Model } from 'mongoose';
+import { RulebookService } from '../rulebook/rulebook.service';
+import { I18nContext, I18nService } from 'nestjs-i18n';
 
 @Injectable()
 export class TagService {
-  create(createTagDto: CreateTagDto) {
-    return 'This action adds a new tag';
+  constructor(
+    @InjectModel(Tag.name) private readonly tagModel: Model<Tag>,
+    private readonly rulebookService: RulebookService,
+    private readonly i18nService: I18nService,
+  ) {}
+
+  translate(path: string) {
+    return this.i18nService.translate(path, {
+      lang: I18nContext.current().lang || 'en',
+    });
   }
 
-  findAll() {
-    return `This action returns all tag`;
+  async create(createTagDto: CreateTagDto): Promise<Tag> {
+    if (
+      await this.tagModel.exists({
+        rulebook: createTagDto.rulebook,
+        name: createTagDto.name,
+      })
+    )
+      throw new HttpException(this.translate('tag.validation.exists'), 400);
+    // ensure rulebook exists
+    await this.rulebookService.findOne(createTagDto.rulebook);
+    return this.tagModel.create(createTagDto);
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} tag`;
+  findAll(
+    page: number,
+    limit: number,
+    rulebook: string,
+    search?: string,
+    sort?: string,
+  ): Promise<Tag[]> {
+    return this.tagModel
+      .find(
+        {
+          rulebook,
+          name: search ? { $regex: search, $options: 'i' } : undefined,
+        },
+        null,
+        {
+          skip: page * limit,
+          limit,
+          sort: sort ? { [sort]: 1 } : undefined,
+        },
+      )
+      .exec();
   }
 
-  update(id: number, updateTagDto: UpdateTagDto) {
-    return `This action updates a #${id} tag`;
+  async findOne(id: string): Promise<Tag> {
+    const tag = await this.tagModel.findById(id).exec();
+    if (!tag)
+      throw new HttpException(this.translate('tag.validation.notFound'), 404);
+    return tag;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} tag`;
+  async update(id: string, updateTagDto: UpdateTagDto): Promise<Tag> {
+    const tag = await this.tagModel.findById(id).exec();
+    if (!tag)
+      throw new HttpException(this.translate('tag.validation.notFound'), 404);
+    // ensure rulebook exists
+    await this.rulebookService.findOne(updateTagDto.rulebook);
+    return tag.set(updateTagDto).save();
+  }
+
+  remove(id: string): Promise<Tag> {
+    if (!this.tagModel.exists({ _id: id }))
+      throw new HttpException(this.translate('tag.validation.notFound'), 404);
+    return this.tagModel.findByIdAndDelete(id).exec();
   }
 }
