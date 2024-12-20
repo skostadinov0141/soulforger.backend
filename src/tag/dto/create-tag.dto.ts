4c849b66e92ad77dffa57dbd39a79cca1ddd6a34
Simeon Kostadinov <simeon.kostadinov0141@gmail.com>
2024-12-20 23:41:24 +0100

    updated create-tag.dto.ts

--- src/tag/dto/create-tag.dto.ts
+++ src/tag/dto/create-tag.dto.ts
@@ -1,1 +1,32 @@
-export class CreateTagDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsOptional, Length } from 'class-validator';
+import { i18nValidationMessage } from 'nestjs-i18n';
+
+export class CreateTagDto {
+  @ApiProperty()
+  @Length(4, 32, {
+    message: i18nValidationMessage('tag.validation.nameLength'),
+  })
+  name: string;
+
+  @ApiProperty()
+  @IsOptional()
+  @Length(4, 32, {
+    message: i18nValidationMessage('tag.validation.colorLength'),
+  })
+  color: string;
+
+  @ApiProperty()
+  @IsOptional()
+  @Length(2, 64, {
+    message: i18nValidationMessage('tag.validation.iconLength'),
+  })
+  icon?: string;
+
+  @ApiProperty()
+  @IsOptional()
+  @Length(4, 128, {
+    message: i18nValidationMessage('tag.validation.descriptionLength'),
+  })
+  description?: string;
+}
