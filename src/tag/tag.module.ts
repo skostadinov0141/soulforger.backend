44ecf061812316888b20dd284068ad3b2200fb2e
Simeon Kostadinov <simeon.kostadinov0141@gmail.com>
2024-12-21 00:07:16 +0100

    added mongoose module to tag.module.ts

--- src/tag/tag.module.ts
+++ src/tag/tag.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { TagService } from './tag.service';
 import { TagController } from './tag.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Tag, TagSchema } from './entities/tag.entity';
+import { RulebookService } from '../rulebook/rulebook.service';
 
 @Module({
   controllers: [TagController],
   providers: [TagService],
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Tag.name,
+        schema: TagSchema,
+      },
+    ]),
+    RulebookService,
+  ],
+  exports: [TagService],
 })
 export class TagModule {}
