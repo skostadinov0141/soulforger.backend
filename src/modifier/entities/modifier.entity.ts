267ea60bce05c949fd3877923eee73cf8efd5d4b
Simeon Kostadinov <simeon.kostadinov0141@gmail.com>
2024-12-24 17:56:54 +0100

    added modifier.entity.ts

--- src/modifier/entities/modifier.entity.ts
+++ src/modifier/entities/modifier.entity.ts
@@ -1,1 +1,89 @@
-export class Modifier {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+@Schema({ timestamps: true })
+export class Modifier {
+  /**
+   * The unique identifier of the modifier.
+   * @example '676acaaaa0c6c947870ae81f'
+   */
+  _id: string;
+
+  /**
+   * The date the modifier was created.
+   * @example '2021-01-01T00:00:00.000Z'
+   */
+  createdAt: Date;
+
+  /**
+   * The date the modifier was last updated.
+   * @example '2021-01-01T00:00:00.000Z'
+   */
+  updatedAt: Date;
+
+  /**
+   * Is the modifier enabled.
+   *
+   * @example true
+   */
+  @Prop({ required: true })
+  enabled: boolean;
+
+  /**
+   * Should the modifier be applied before the target's base modifier.
+   * @example true
+   */
+  @Prop({ required: true })
+  beforeModifier: boolean;
+
+  /**
+   * The target path of the modifier.
+   * @example 'character.attributes'
+   */
+  @Prop({ required: true })
+  targetPath: string; // TODO change to PathEntity ENUM
+
+  /**
+   * The unique identifier of the target that should be modified.
+   * @example '676acaaaa0c6c947870ae81f'
+   */
+  @Prop({ required: true })
+  targetId: string;
+
+  /**
+   * The source path of the source that created the modifier.
+   * @example 'character.attributes'
+   */
+  @Prop({ required: true })
+  sourcePath: string; // TODO change to PathEntity ENUM
+
+  /**
+   * The unique identifier of the source that created the modifier.
+   * @example '676acaaaa0c6c947870ae81f'
+   */
+  @Prop({ required: true })
+  sourceId: string;
+
+  /**
+   * The mode of the modifier.
+   * @example 'add'
+   */
+  @Prop({
+    required: true,
+    enum: ['add', 'subtract', 'divide', 'multiply', 'replace'],
+  })
+  mode: 'add' | 'subtract' | 'divide' | 'multiply' | 'replace';
+
+  /**
+   * The inverse mode of the modifier.
+   * @example 'subtract'
+   */
+  @Prop({
+    required: true,
+    enum: ['add', 'subtract', 'multiply', 'replace'],
+  })
+  inverseMode: 'add' | 'subtract' | 'multiply' | 'replace';
+}
+
+export const ModifierSchema = SchemaFactory.createForClass(Modifier);
+export type ModifierDocument = HydratedDocument<Modifier>;
