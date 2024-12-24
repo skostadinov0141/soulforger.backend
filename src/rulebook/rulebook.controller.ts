c0e05f37b4dca42b654ff24d3a4aad492bde7414
Simeon Kostadinov <simeon.kostadinov0141@gmail.com>
2024-12-24 01:05:51 +0100

    removed unneeded decorators from rulebook.controller.ts

--- src/rulebook/rulebook.controller.ts
+++ src/rulebook/rulebook.controller.ts
@@ -11,7 +11,7 @@ import {
 import { RulebookService } from './rulebook.service';
 import { CreateRulebookDto } from './dto/create-rulebook.dto';
 import { UpdateRulebookDto } from './dto/update-rulebook.dto';
-import { ApiParam, ApiQuery, ApiTags } from '@nestjs/swagger';
+import { ApiTags } from '@nestjs/swagger';
 import { Rulebook } from './entities/rulebook.entity';
 import { LocaleQuery } from '../locale-query/locale-query.decorator';
 
@@ -43,8 +43,6 @@ export class RulebookController {
    * @throws {500} Internal Server Error
    */
   @Get()
-  @ApiQuery({ name: 'page', required: false, type: Number, example: 0 })
-  @ApiQuery({ name: 'limit', required: false, type: Number, example: 10 })
   findAll(
     @Query('page') page: number = 0,
     @Query('limit') limit: number = 10,
@@ -60,7 +58,6 @@ export class RulebookController {
    * @throws {500} Internal Server Error
    */
   @Get(':id')
-  @ApiParam({ name: 'id', type: String, example: '6768691c0767ae795feb2c60' })
   findOne(@Param('id') id: string): Promise<Rulebook> {
     return this.rulebookService.findOne(id);
   }
@@ -73,7 +70,6 @@ export class RulebookController {
    * @throws {500} Internal Server Error
    */
   @Patch(':id')
-  @ApiParam({ name: 'id', type: String, example: '6768691c0767ae795feb2c60' })
   update(
     @Param('id') id: string,
     @Body() updateRulebookDto: UpdateRulebookDto,
@@ -89,7 +85,6 @@ export class RulebookController {
    * @throws {500} Internal Server Error
    */
   @Delete(':id')
-  @ApiParam({ name: 'id', type: String, example: '6768691c0767ae795feb2c60' })
   remove(@Param('id') id: string): Promise<Rulebook> {
     return this.rulebookService.remove(id);
   }
