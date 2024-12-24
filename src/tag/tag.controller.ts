4180b4e64c92c0bf99ae1eaebc5b410885922288
Simeon Kostadinov <simeon.kostadinov0141@gmail.com>
2024-12-24 01:08:17 +0100

    removed unneeded decorators from tag.controller.ts

--- src/tag/tag.controller.ts
+++ src/tag/tag.controller.ts
@@ -32,55 +32,11 @@ export class TagController {
    * @throws
    */
   @Post()
-  @ApiBody({ type: CreateTagDto })
   create(@Body() createTagDto: CreateTagDto): Promise<Tag> {
     return this.tagService.create(createTagDto);
   }
 
   @Get()
-  @ApiQuery({
-    name: 'page',
-    required: false,
-    type: Number,
-    description: 'Page number',
-  })
-  @ApiQuery({
-    name: 'limit',
-    required: false,
-    type: Number,
-    description: 'Number of items per page',
-  })
-  @ApiQuery({
-    name: 'rulebook',
-    required: true,
-    type: String,
-    description: 'Rulebook ID',
-  })
-  @ApiQuery({
-    name: 'search',
-    required: false,
-    type: String,
-    description: 'Search query',
-  })
-  @ApiQuery({
-    name: 'order',
-    required: false,
-    type: String,
-    enum: ['asc', 'desc'],
-    description: 'Sort order',
-  })
-  @ApiQuery({
-    name: 'sort',
-    required: false,
-    type: String,
-    description: 'Sort by field',
-  })
-  @ApiQuery({
-    name: 'populateRulebook',
-    required: false,
-    type: Boolean,
-    description: 'Populate rulebook',
-  })
   findAll(
     @Query('page') page: number = 0,
     @Query('limit') limit: number = 10,
@@ -102,34 +58,16 @@ export class TagController {
   }
 
   @Get(':id')
-  @ApiParam({
-    name: 'id',
-    type: String,
-    description: 'Tag ID',
-    example: '6768691c0767ae795feb2c60',
-  })
   findOne(@Param('id') id: string) {
     return this.tagService.findOne(id);
   }
 
   @Patch(':id')
-  @ApiParam({
-    name: 'id',
-    type: String,
-    description: 'Tag ID',
-    example: '6768691c0767ae795feb2c60',
-  })
   update(@Param('id') id: string, @Body() updateTagDto: UpdateTagDto) {
     return this.tagService.update(id, updateTagDto);
   }
 
   @Delete(':id')
-  @ApiParam({
-    name: 'id',
-    type: String,
-    description: 'Tag ID',
-    example: '6768691c0767ae795feb2c60',
-  })
   remove(@Param('id') id: string) {
     return this.tagService.remove(id);
   }
