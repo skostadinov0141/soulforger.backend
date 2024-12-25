31df26c4a47f1a1ab145b8e9bc4c453c2f6510d8
Simeon Kostadinov <simeon.kostadinov0141@gmail.com>
2024-12-25 01:09:07 +0100

    added create and update modifier dto

--- src/modifier/dto/update-modifier.dto.ts
+++ src/modifier/dto/update-modifier.dto.ts
@@ -1,4 +1,11 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateModifierDto } from './create-modifier.dto';
 
-export class UpdateModifierDto extends PartialType(CreateModifierDto) {}
+export class UpdateModifierDto extends PartialType(CreateModifierDto) {
+  /**
+   * Is the modifier enabled.
+   *
+   * @example true
+   */
+  enabled: boolean;
+}
