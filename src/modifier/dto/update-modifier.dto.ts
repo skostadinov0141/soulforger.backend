9d421e5134104d3d457b012a7f6abbd6fc6d985e
Simeon Kostadinov <simeon.kostadinov0141@gmail.com>
2024-12-25 19:16:55 +0100

    removed modifier due to a different approach being found

--- src/modifier/dto/update-modifier.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateModifierDto } from './create-modifier.dto';
-
-export class UpdateModifierDto extends PartialType(CreateModifierDto) {}
