047c285a9a0ab23f8774df8f739ee2ccc1eedb97
Simeon Kostadinov <simeon.kostadinov0141@gmail.com>
2024-12-25 19:25:32 +0100

    added character-template.entity.ts

--- src/character-template/entities/character-template.entity.ts
+++ src/character-template/entities/character-template.entity.ts
@@ -1,1 +1,16 @@
-export class CharacterTemplate {}
+import { Schema } from '@nestjs/mongoose';
+import { Tag } from '../../tag/entities/tag.entity';
+
+@Schema({ timestamps: true })
+export class CharacterTemplate {
+  _id: string;
+  createdAt: Date;
+  updatedAt: Date;
+  name: string;
+  description: string;
+  tags: Tag[];
+  attributes: any;
+  derivedAttributes: any;
+  skills: any;
+  items: any;
+}
