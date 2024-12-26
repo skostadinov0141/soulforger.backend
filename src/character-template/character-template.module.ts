193f8d1f3aaa52f97aa3e7098c1c73e174617c1d
Simeon Kostadinov <simeon.kostadinov0141@gmail.com>
2024-12-27 00:21:38 +0100

    added needed imports and exports to character-template.module.ts

--- src/character-template/character-template.module.ts
+++ src/character-template/character-template.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
 import { CharacterTemplateService } from './character-template.service';
 import { CharacterTemplateController } from './character-template.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import {
+  CharacterTemplate,
+  CharacterTemplateSchema,
+} from './entities/character-template.entity';
+import { RulebookService } from '../rulebook/rulebook.service';
+import { TagService } from '../tag/tag.service';
 
 @Module({
   controllers: [CharacterTemplateController],
   providers: [CharacterTemplateService],
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: CharacterTemplate.name,
+        schema: CharacterTemplateSchema,
+      },
+    ]),
+    RulebookService,
+    TagService,
+  ],
+  exports: [CharacterTemplateService],
 })
 export class CharacterTemplateModule {}
