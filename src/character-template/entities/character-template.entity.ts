b4e0c086e45ad6fc0b83000ac37e42aeeb9588a5
Simeon Kostadinov <simeon.kostadinov0141@gmail.com>
2024-12-27 00:16:45 +0100

    added prop definitions to character-template.entity.ts

--- src/character-template/entities/character-template.entity.ts
+++ src/character-template/entities/character-template.entity.ts
@@ -1,4 +1,4 @@
-import { Schema } from '@nestjs/mongoose';
+import { Prop, Schema } from '@nestjs/mongoose';
 import { Tag } from '../../tag/entities/tag.entity';
 import { Rulebook } from '../../rulebook/entities/rulebook.entity';
 import { Attribute } from './attribute.entity';
@@ -10,13 +10,31 @@ export class CharacterTemplate {
   _id: string;
   createdAt: Date;
   updatedAt: Date;
+
+  @Prop({ required: true })
   name: string;
+
+  @Prop({ required: true })
   description: string;
+
+  @Prop({ type: [Tag], required: true, ref: Tag.name })
   tags: Tag[];
+
+  @Prop({ required: true, ref: Rulebook.name })
   rulebook: Rulebook;
+
+  @Prop({ type: [Attribute], required: true })
   attributes: Attribute[];
+
+  @Prop({ type: [Property], required: true })
   properties: Property[];
+
+  @Prop({ type: [DerivedAttribute], required: true })
   derivedAttributes: DerivedAttribute[];
+
+  @Prop({ required: true })
   skills: any;
+
+  @Prop({ required: true })
   items: any;
 }
