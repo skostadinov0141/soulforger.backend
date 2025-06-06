ec92d410ca1d9035025fdaaabcc6058996fe4c13
Simeon Kostadinov <simeon.kostadinov0141@gmail.com>
2025-06-06 14:26:43 +0200

    refactor: improve JSDoc comments for character properties and modifiers

--- src/core/entities/character/character.entity.ts
+++ src/core/entities/character/character.entity.ts
@@ -8,14 +8,20 @@ export class Character {
   createdAt: Date;
   updatedAt: Date;
 
-  /*
+  /**
    * The properties that define the character.
+   * These properties can include attributes like strength, agility, intelligence, etc.
+   * Each property is an instance of CharacterProperty.
+   * @type {CharacterProperty[]}
    */
   @Prop({ type: [CharacterProperty] })
   properties: CharacterProperty[];
 
-  /*
+  /**
    * The modifiers that change the character's properties.
+   * These modifiers can include buffs, debuffs, or other effects that alter the character's stats.
+   * Each modifier is an instance of CharacterModifier.
+   * @type {Record<string, CharacterModifier[]>}
    */
   @Prop({ type: Object })
   modifiers: Record<string, CharacterModifier[]>;
